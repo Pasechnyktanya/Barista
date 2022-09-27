bbf1863f7da2e4ea01e21e877d7e10c3d69f95e9
Pasechnyktanya <pasechnyktanya@gmail.com>
2022-09-28 02:02:57 +0300

    added css, correct btn

---
+++ js/pr-carousel.js
@@ -0,0 +1,51 @@
+(function () {
+
+    const slides = [
+        `<div class="ws__card">
+                <div class="ws__card__icon"></div>
+                <div class="ws__card__name">JOHN JELLY</div>
+                <div class="ws__card__course"><em>Coffee Enthusiast</em></div>
+                <div class="ws__content">“I am the kind of person that can sing odes to coffee. At the
+                        course, I learned to make the perfect coffee at home, so now I have no reason to leave it at
+                        all. I can't even count how much money I’m saving on coffee-to-go every day!”</div>
+                <div class="ws__data">December 15, 2021</div>
+            </div>`,
+        `<div class="ws__card">
+                    <div class="ws__card__icon"></div>
+                    <div class="ws__card__name">MARY WOLLEN</div>
+                    <div class="ws__card__course"><em>Barista</em></div>
+                    <div class="ws__content">“I was so happy to study at CoffeeLab. They know how to work
+                        with
+                        poor learners and find the best approach to them. Sure, I still have many tricks to learn,
+                        but
+                        this school gave me basic skills that I use every day at work.”</div>
+                    <div class="ws__data">March 15, 2021</div>`,
+        `<div class="ws__card">
+                    <div class="ws__card__icon"></div>
+                    <div class="ws__card__name">LILLY BLUES</div>
+                    <div class="ws__card__course"><em>Bar Owner</em></div>
+                    <div class="ws__content">“It changed the way I'm doing my business. The courses gave me
+                        the
+                        management basics for my own coffee bar and the community gave the best baristas. Running my
+                        coffee place is now such a pleasure.”</div>
+                    <div class="ws__data">November 22, 2021</div>
+                </div>`
+    ];
+
+    let slideIdx = 0;
+    const slideContainer = document.querySelector(`.pr-carousel .slide-container`);
+
+    function showCurrentSlide() {
+       
+        slideContainer.innerHTML = slides[slideIdx];
+
+    }
+
+    function nextSlide() {
+        slideIdx = slideIdx + 1 >= slides.length ? 0 : slideIdx + 1;
+        showCurrentSlide();
+    }
+
+    setInterval(nextSlide, 3000);
+
+})()
