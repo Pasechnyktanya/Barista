315ca95d2fb743355e8dac7c261aa9d1cf9fd27a
Elena Tarasova <alissy@i.ua>
2022-09-29 20:15:42 +0300

    added strings to price in product-list.js

--- js/app-pricing-carousel.js
+++ js/app-pricing-carousel.js
@@ -5,25 +5,46 @@
 
             id: 1,
             name: 'LATTE ART COURSE',
-            price: '$150 | 5 days'
+            price: '$150 | 5 days',
+            string_1: 'For all coffee fans;',
+            string_2: 'Offline lessons only;',
+            string_3: 'All materials included;',    
+            string_4: '8 hours of training;',
+            string_5: 'Easy to repeat at home;'
+
         
         },
         {
             id: 2,
             name: 'BARISTA LICENSE COURSE',
             price: '$365 | 23 days',
+            string_1: 'For beginners;',
+            string_2: '50+ hours of training;',
+            string_3: 'Classic drinks;',    
+            string_4: 'Interships included;',
+            string_5: 'A certificate is issued;'
 
         },
         {
             id: 3,
             name: 'ADVANCED BARISTA COURSE',
             price: '$350 | 14 days',
+            string_1: 'For proffessionals;',
+            string_2: 'Online and offline;',
+            string_3: 'Proffessional equipment;',    
+            string_4: 'Coffee roasting details;',
+            string_5: 'Alternative brewing;'
 
         },
         {
             id: 4,
             name: 'COFFEE LAKE A PRO AT HOME',
-            price: '$150 | 5 days'
+            price: '$150 | 5 days',
+            string_1: 'For beginners;',
+            string_2: 'Classic recipes;',
+            string_3: 'Secret tips;',    
+            string_4: 'Making lush milk foam;',
+            string_5: '10 hours of training;'
 
         }
     ]
@@ -43,15 +64,15 @@
             </span>
             <ul class="list__about__course">
                 <li class="cards__list"><img class="img__checked__menu" src="img/checked__menu.svg"
-                        alt="checked menu" class="img__checked__menu"> For all coffee fans;</li>
+                        alt="checked menu" class="img__checked__menu">${product.string_1}</li>
                 <li class="cards__list"><img class="img__checked__menu" src="img/checked__menu.svg"
-                        alt="checked menu" class="img__checked__menu"> Offline lessons only;</li>
+                        alt="checked menu" class="img__checked__menu">${product.string_2}</li>
                 <li class="cards__list"><img class="img__checked__menu" src="img/checked__menu.svg"
-                        alt="checked menu" class="img__checked__menu"> All materials included;</li>
+                        alt="checked menu" class="img__checked__menu"> ${product.string_3}</li>
                 <li class="cards__list"><img class="img__checked__menu" src="img/checked__menu.svg"
-                        alt="checked menu" class="img__checked__menu"> 8 hours of training;</li>
+                        alt="checked menu" class="img__checked__menu">${product.string_4}</li>
                 <li class="cards__list"><img class="img__checked__menu" src="img/checked__menu.svg"
-                        alt="checked menu" class="img__checked__menu"> Easy to repeat at home;</li>
+                        alt="checked menu" class="img__checked__menu">${product.string_5}</li>
             </ul>
             <button class="button__pricing">
                 Select Course
